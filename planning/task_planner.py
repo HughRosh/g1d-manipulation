3b71040a6690e4c778b81ca8dbcf985b55733acb
HughRosh <Hugh@americanrobots.com>
2026-07-08 21:21:43 -0400

    Add task planner skeleton

---
+++ planning/task_planner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Task Planner
+
+Converts abstract tasks into executable motion commands.
+"""
+
+from planning.tasks.tasks import *
+
+
+class TaskPlanner:
+
+    def solve(self, task):
+
+        print(f"Planning task: {task}")
+
+        return []
